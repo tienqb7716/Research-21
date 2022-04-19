8a1ef63d66bb3d45dc7febf900b17f5c2fbb9219
Tien <tientv.63.student@fit.tdc.edu.vn>
2022-04-19 22:45:46 +0700

    update file

--- app/main.js
+++ app/main.js
@@ -51,7 +51,6 @@ let html = ` <div class="wrapper-rs">
 `
 // nhung link bootstrap
 document.head.insertAdjacentHTML('afterbegin', `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">`);
-document.head.insertAdjacentHTML('afterbegin', `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">`);
 document.body.insertAdjacentHTML('afterbegin', html);
 const wrapper = document.querySelector(".wrapper-rs"),
   header = wrapper.querySelector(".wrapper-rs>header");

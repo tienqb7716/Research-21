92594308c7ae6dfd9be34dee5b2bc7d6b6b6ebed
Tien <tientv.63.student@fit.tdc.edu.vn>
2022-04-20 19:56:57 +0700

    update chuc nang

--- app/js/printTablePerson.js
+++ app/js/printTablePerson.js
@@ -1,8 +1,9 @@
 
 const tboby = document.querySelector('.table tbody')
+let btn = document.querySelector('.export-to-excel');
 
 chrome.storage.sync.get(['tablePerson'], function (result) {
-  if (result.tablePerson!=null) {
+  if (result.tablePerson != null) {
     const tablePerson = JSON.parse(result.tablePerson)
     console.log(tablePerson);
     let i = 1;
@@ -16,5 +17,17 @@ chrome.storage.sync.get(['tablePerson'], function (result) {
         `
     });
     chrome.storage.sync.remove(['tablePerson']);
+    btn.classList.remove('d-none');
   }
 });
+function ExportToExcel(type, fn, dl) {
+  var elt = document.getElementById('table');
+  var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+  return dl ?
+    XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
+    XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
+}
+
+btn.addEventListener('click',function () {
+  ExportToExcel('xlsx');
+})
